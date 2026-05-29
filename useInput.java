9d9cdc3b57c12dd2fe931f91c459f8a381fa923d
Yash bhavsar <bhavsaryash823@gmail.com>
2026-05-29 17:02:29 +0530

    Add datatypes

--- useInput.java
+++ useInput.java
@@ -1,5 +1,6 @@
 import custominput.*;
-public class useInput extends CharInput {
+// Example of char input
+public class useInput extends CharInput{
     public static void main(String a[]){
         char chr= input("Enter char:  ");
         System.out.println(chr);
